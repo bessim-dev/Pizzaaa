5634805cf29bdfbc086e6c390d8ca6f755cdc2dd
becem-dev <you@example.com>
2020-10-22 10:33:12 +0100

    simple animation inside every component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Home = () => {
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      textShadow: "0px 0px 8px rgb(255,255,255)",
+      boxShadow: "0px 0px 8px rgb(255,255,255)",
+      transition: {
+        yoyo: Infinity,
+        duration: 0.3,
+      },
+    },
+  };
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 1.5 }}
+      className="home container"
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <button>
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 const Order = ({ pizza }) => {
+  const [showTitle, toggle] = useState(true);
+  setTimeout(() => toggle(false), 1000);
+  console.log(showTitle);
   return (
     <div className="container order">
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
+
       <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      {pizza.toppings.map((topping) => (
+        <div key={topping}>{topping}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Order;
